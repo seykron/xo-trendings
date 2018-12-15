4e5fab53252894737efda742a51606ad3ba77152
Matías Mirabelli <matias.mi+xo@riseup.net>
2018-12-15 14:50:05 -0300

    Fixes bug that prevented users to select more than 50 videos per page.

--- src/services/youtube/Youtube.js
+++ src/services/youtube/Youtube.js
@@ -1,26 +1,58 @@
 import Axios from 'axios';
 import {appConfig} from '../../config';
 import {VideoClass} from '../../models/video.class';
+import { isNullOrUndefined } from 'util';
 
 const axios = Axios.create({
   baseURL: appConfig.getYoutubeEndPoint('videos')
 });
 
-export class YoutubeService {
-  getTrendingVideos(videosPerPage = appConfig.maxVideosToLoad) {
-    const params = {
-      part: appConfig.partsToLoad,
-      chart: appConfig.chart,
-      videoCategoryId: appConfig.defaultCategoryId,
-      regionCode: appConfig.defaultRegion,
-      maxResults: videosPerPage,
-      key: appConfig.youtubeApiKey
-    };
+const MAX_VIDEOS_PER_PAGE = 50;
 
-    return axios.get('/', {params}).then((res) => {
-      return res.data.items
-        .map((item) => new VideoClass(item))
-        .filter((item) => item.id !== '');
-    }).catch((err) => err);
+function createListParams(videosPerPage = appConfig.maxVideosToLoad,
+                          pageToken) {
+  return {
+    part: appConfig.partsToLoad,
+    chart: appConfig.chart,
+    videoCategoryId: appConfig.defaultCategoryId,
+    regionCode: appConfig.defaultRegion,
+    maxResults: videosPerPage < MAX_VIDEOS_PER_PAGE ? videosPerPage : MAX_VIDEOS_PER_PAGE,
+    key: appConfig.youtubeApiKey,
+    pageToken: pageToken
+  };
+}
+
+/** Gets most popular videos from the Youtube data API.
+ * 
+ * @param {Number} videosPerPage Number of videos per page.
+ * @param {String} [pageToken] A page token to fetch the next page, used for pagination, should 
+ * not be used from outside this function.
+ * @param {VideoClass[]} [videos] List of already resolved videos. Used for pagination, should
+ * not be used outside this function.
+ */
+async function fetchVideosPage(videosPerPage = appConfig.maxVideosToLoad, pageToken, videos = []) {
+  const params = createListParams(videosPerPage, pageToken);
+  const result = await axios.get('/', {params});
+  const nextPageToken = result.data.nextPageToken;
+  const nextVideos = videos.concat(
+    result.data.items
+      .map(item => new VideoClass(item))
+      .filter(item => item.id !== '')
+  );
+
+  if (nextVideos.length < videosPerPage && !isNullOrUndefined(nextPageToken)) {
+    return await fetchVideosPage(
+      videosPerPage,
+      nextPageToken,
+      nextVideos
+    );
+  } else {
+    return nextVideos.slice(0, videosPerPage);
+  }
+}
+
+export class YoutubeService {
+  async getTrendingVideos(videosPerPage = appConfig.maxVideosToLoad) {
+    return await fetchVideosPage(videosPerPage);
   }
 }
--- src/services/youtube/Youtube.test.js
+++ src/services/youtube/Youtube.test.js
@@ -5,8 +5,20 @@ it('renders without crashing', () => {
  new YoutubeService();
 });
 
-it('getTrendingVideos function', async () => {
+it('getTrendingVideos function with default configuration', async () => {
   const service = new YoutubeService();
   const result = await service.getTrendingVideos();
   expect(result.length).toEqual(24);
+});
+
+it('getTrendingVideos function with more than 50 videos', async () => {
+  const service = new YoutubeService();
+  const result = await service.getTrendingVideos(57);
+  expect(result.length).toEqual(57);
+});
+
+it('getTrendingVideos function with more than maximum videos allowed by Youtube', async () => {
+  const service = new YoutubeService();
+  const result = await service.getTrendingVideos(250);
+  expect(result.length).toEqual(200);
 });
